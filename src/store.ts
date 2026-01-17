6407c748e7ca7a02ca06e7281b2474b5e766242c
anoop-dignizant <anoop@dignizant.com>
2026-01-17 18:20:54 +0530

    fix: revert getUsersasdasds function to return simple sample data

--- src/store.ts
+++ src/store.ts
@@ -5,18 +5,7 @@ const users: User[] = [];
 
 export const getUsersasdasds = (): any => {
 
-  return {
-    hello: "world",
-    users: users,
-    killer: "bee",
-    heythere:"sfgas",
-    kngks:"sdfsfd",
-    number: 123456,
-    sdfsdfasdf:"adfasdfd",
-    sdfada:"sdfasdfd",
-    asdasdasdasda:"asdasdasd",
-    dsfjisjfisdjf:"sdfadfsdfsd"
-  };
+  return "Sample Data";
 }; // Reverted to return User[] for simplicity
 const eeeeeeeeeeee = 1234567890;
 
